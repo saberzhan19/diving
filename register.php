c4639dd5c51ad8a9a693f23fb80ac4bf37a5b00e
Saberzhan19 <Saberzhax@gmail.com>
2021-12-09 05:48:19 +0600

    roots

---
+++ register.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+require "functions.php";
+
+
+$email = $_SESSION["email"];
+$password = $_SESSION["password"];
+
+$dive = get_user_by_email($email);
+
+// если эл. адрес занят, то перенаправляем назад
+
+if(!empty($dive)){
+    set_flash_message("danger", "Этот эл. адрес уже занят другим пользователем.");
+    redirect_to("page_register.php");
+}
+
+add_user($email, $password);
+
+set_flash_message("success", "Регистрация успешна");
+
+redirect_to("/page_login.php");
